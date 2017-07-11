0f08c1bf5aff6cf8297886e0431eeaeddcdf362e
moonburni <2047311885@qq.com>
2017-07-11 17:20:57 +0800

    个人信息修改

--- js/babel_js.js
+++ js/babel_js.js
@@ -7,22 +7,42 @@ var dialogDomain = Object.create(null);
 dialogDomain.loadingText = "<span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span>";
 
 function goOn(state) {
-    if (state === '你是谁？' || state === "能介绍一下你自己吗？") {
+    if (state === 1) {
         scrollMove("O(∩_∩)O", dialogBuilder, "我是广州中医药大学", dialogBuilder, "计算机专业的大四学生", dialogBuilder, "特别喜欢前端~", dialogBuilder, ["除了前端还有什么兴趣爱好？", "详细一点可以吗？"], answerBodyBuilder);
-    } else if (state === '这是什么？') {
+    } else if (state === 2) {
         scrollMove("额...", dialogBuilder, "这个是我模仿一款叫做《异次元通讯》的手机文字游戏做的", dialogBuilder, "是不是很特别呀,哈哈", dialogBuilder, ["能介绍一下你自己吗？", "做这个的目的是什么？"], answerBodyBuilder);
-    } else if (state === '做这个的目的是什么？') {
+    } else if (state === 3) {
         scrollMove("...", dialogBuilder, "不瞒您说，我是想找一份有关前端实习的工作（捂脸）", dialogBuilder, "又不知道做什么", dialogBuilder, "觉得这个挺好玩的，就像试试看", dialogBuilder, ["还有其他东西吗？", "介绍一下你吧"], answerBodyBuilder);
-    } else if (state === "能介绍一下你自己吗？" || state === "详细一点可以吗？" || state === "介绍一下你吧") {
+    } else if (state === 4) {
         scrollMove("性别男", dialogBuilder, "爱好...前端", dialogBuilder, "广州中医药计算机专业大四学生", dialogBuilder, "<a href='https://github.com/Moonburni'>我的gitHub</a>", dialogBuilder, "<a href='http://www.jianshu.com/u/4ce5e6053736'>我的简书</a>", dialogBuilder, ["不错!怎么联系你？", "再见，对你不感兴趣"], answerBodyBuilder);
-    } else if (state === "除了前端还有什么兴趣爱好？") {
+    } else if (state === 5) {
         scrollMove("我喜欢唱歌", dialogBuilder, "喜欢音乐", dialogBuilder, "还有看书，各种书~各种类型的书", dialogBuilder, ["介绍一下你吧", "再见，对你不感兴趣"], answerBodyBuilder);
-    } else if (state === "再见，对你不感兴趣") {
+    } else if (state === 6) {
         scrollMove("好吧，再见....", dialogBuilder, false, online);
-    } else if (state === "不错!怎么联系你？" || state === "快到碗里来!") {
+    } else if (state === 7) {
         scrollMove("我的电子邮箱是：", dialogBuilder, "2047311885@qq.com", dialogBuilder, "我的手机是：", dialogBuilder, "13535452921", dialogBuilder, "求勾搭，求联系~", dialogBuilder, false, online);
-    } else if (state === "还有其他东西吗？") {
+    } else if (state === 8) {
         scrollMove("<a href='https://github.com/Moonburni'>我的gitHub</a>", dialogBuilder, "<a href='http://www.jianshu.com/u/4ce5e6053736'>我的简书</a>", dialogBuilder, ["不错!怎么联系你？", "再见，对你不感兴趣"], answerBodyBuilder);
+    }
+}
+
+function selectState(state) {
+    if (state === '你是谁？' || state === "能介绍一下你自己吗？") {
+        return 1;
+    } else if (state === '这是什么？') {
+        return 2;
+    } else if (state === '做这个的目的是什么？') {
+        return 3;
+    } else if (state === "能介绍一下你自己吗？" || state === "详细一点可以吗？" || state === "介绍一下你吧") {
+        return 4;
+    } else if (state === "除了前端还有什么兴趣爱好？") {
+        return 5;
+    } else if (state === "再见，对你不感兴趣") {
+        return 6;
+    } else if (state === "不错!怎么联系你？" || state === "快到碗里来!") {
+        return 7;
+    } else if (state === "还有其他东西吗？") {
+        return 8;
     }
 }
 
@@ -76,7 +96,7 @@ function answerBodyBuilder(text) {
         answerBody.style.display = 'none';
         answerBody.style.height = '0';
         setTimeout(function () {
-            goOn(text[0]);
+            goOn(selectState(text[0]));
         }, 1000);
     };
     answerContent2.onclick = function () {
@@ -84,7 +104,7 @@ function answerBodyBuilder(text) {
         answerBody.style.display = 'none';
         answerBody.style.height = '0';
         setTimeout(function () {
-            goOn(text[1]);
+            goOn(selectState(text[1]));
         }, 1000);
     };
 }
